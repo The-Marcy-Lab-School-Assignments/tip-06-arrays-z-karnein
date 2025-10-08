d22ab94936e2553d0a81eecbe57080bf58fcc158
zoulkarnein <zoulkarneinbagagnan@gmail.com>
2025-10-08 11:16:11 -0400

    wrote finrMax function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+const findMax = (numbers) =>{
+    let firstNum = numbers[0]
+    for (let i = 0; i <= numbers.length; i++ ){
+       if (numbers[i]  > firstNum){
+        firstNum = numbers[i]
+       }
+    }
+return firstNum
+}
+
+console.log(findMax([3, 7, 2, 9, 5])); // ➜ 9
+console.log(findMax([-1, -5, -3]));
+
+// Write a `findMax` function that takes an **array of numbers** as input and returns the **largest number** in that array.
